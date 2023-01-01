f9f43e00461fb94731d95970c14622089c13c395
justinkim2015 <justinkim2015@gmail.com>
2023-01-02 05:56:53 +0900

    Add title to about page

--- src/aboutme/About.js
+++ src/aboutme/About.js
@@ -7,7 +7,10 @@ const AboutMe = () => {
     <div id='about-main'>
       <div id="about-box">
         <Back />
-        <div className="container"><img src={Picture} alt='profile' id='profile-pic'></img></div>
+        <div className="container">
+          <h3>Hi, I made this,</h3>
+          <img src={Picture} alt='profile' id='profile-pic'></img>
+        </div>
         <p id='article'>
           This app was made with React and styled vanilla CSS3.  This was made to practice API integration with React and to create single page applications.  
           <br/><br/>
